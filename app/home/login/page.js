2243588c71f98204115ee0467eec63aa0bb26de6
kaloyanTry <kaloyantryavna@gmail.com>
2024-08-11 10:54:55 +0300

    refactor login page

--- app/home/login/page.js
+++ app/home/login/page.js
@@ -5,8 +5,10 @@ export const metadata = {
 };
 
 async function LoginPage() {
-  const res = await fetch('http://localhost:3000/api/auth/providers');
-  // const res = await fetch('./../../api/auth/providers');
+  // const res = await fetch('http://localhost:3000/api/auth/providers');
+  const res = await fetch(
+    'https://balkana-homepage-git-main-kaloyantrys-projects.vercel.app/api/auth/callback/google/api/auth/providers'
+  );
   const data = await res.json();
   const providers = Object.keys(data);
 
