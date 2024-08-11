8238808de9b30e9262ce745ae8ba436e22c690ae
kaloyanTry <kaloyantryavna@gmail.com>
2024-08-11 10:50:49 +0300

    login re

--- app/home/login/page.js
+++ app/home/login/page.js
@@ -6,7 +6,7 @@ export const metadata = {
 
 async function LoginPage() {
   // const res = await fetch('http://localhost:3000/api/auth/providers');
-  const res = await fetch('/api/auth/providers');
+  const res = await fetch('./../../api/auth/providers');
   const data = await res.json();
   const providers = Object.keys(data);
 
