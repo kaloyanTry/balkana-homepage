032c38c317b7af29c926457e5ecb53c46c56ce48
kaloyanTry <kaloyantryavna@gmail.com>
2024-08-25 15:15:42 +0300

    Slideshow arrows styled

--- components/HomeImagesShow.js
+++ components/HomeImagesShow.js
@@ -1,10 +1,7 @@
 'use client';
 import { useState, useEffect } from 'react';
 
-import {
-  ArrowLeftCircleIcon,
-  ArrowRightCircleIcon,
-} from '@heroicons/react/24/solid';
+import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/solid';
 
 // Another approuch of coding and displaying an image slideshow without using react-slideshow-image:
 
@@ -43,13 +40,13 @@ const HomeImagesShow = ({ images }) => {
 
   let Arrow = ({ direction, onClick }) => (
     <div
-      className='h-8 w-8 text-primary-100  group-hover:block absolute top-[50%] translate-y-[-50%] cursor-pointer'
+      className='h-8 w-8 text-primary-100 bg-primary-100 bg-opacity-10 rounded-sm  group-hover:block absolute top-[50%] translate-y-[-50%] cursor-pointer'
       style={{ [direction]: '40px' }}
     >
       {direction === 'left' ? (
-        <ArrowLeftCircleIcon onClick={onClick} />
+        <ArrowLeftIcon onClick={onClick} />
       ) : (
-        <ArrowRightCircleIcon onClick={onClick} />
+        <ArrowRightIcon onClick={onClick} />
       )}
     </div>
   );
@@ -58,7 +55,7 @@ const HomeImagesShow = ({ images }) => {
     <main className='m-auto py-4 relative group'>
       <article
         style={{ backgroundImage: `url(${trackImages[currentIndex].url})` }}
-        className='w-auto h-screen max-h-[60vh] min-h-96 bg-center bg-cover placeholder-primary-100 transition-all'
+        className='w-auto h-screen max-h-[60vh] rounded-sm min-h-96 bg-center bg-cover placeholder-primary-100 transition-all'
         key={trackImages[currentIndex]}
       >
         <Arrow direction='left' onClick={prevImage} />
@@ -69,7 +66,7 @@ const HomeImagesShow = ({ images }) => {
             {trackImages.map((_, i) => (
               <div
                 key={i}
-                className={`transition-all ease-out duration-800 w-2 h-2 bg-primary-100 rounded-full ${
+                className={`transition-all ease-out duration-800 w-2 h-2 bg-primary-100 rounded-sm ${
                   currentIndex === i ? 'p-1' : 'bg-opacity-50'
                 }`}
               ></div>
