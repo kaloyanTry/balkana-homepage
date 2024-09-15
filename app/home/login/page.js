20ee253c963cc4654771305972ab3015c8616de0
kaloyanTry <kaloyantryavna@gmail.com>
2024-09-15 14:29:39 +0300

    home login page

--- app/home/login/page.js
+++ app/home/login/page.js
@@ -4,23 +4,23 @@ export const metadata = {
   title: 'Login to BalkanaTry App',
 };
 
-async function LoginPage() {
+export default function LoginPage() {
   // const res = await fetch('http://localhost:3000/api/auth/providers');
-  const res = await fetch('https://balkana-try.vercel.app/api/auth/providers');
-  const data = await res.json();
-  const providers = Object.keys(data);
+  // const res = await fetch('https://balkana-try.vercel.app/api/auth/providers');
+  // const data = await res.json();
+  // const providers = Object.keys(data);
 
   return (
     <div className='flex flex-col gap-8 mt-8 items-center'>
       <h1 className='text-4xl font-semibold text-primary-300 mb-4'>
         Sign in to access your explorations&apos; area
       </h1>
+
       <SignInBtn />
+
       {/* {providers.map((provider) => (
         <SignInBtn provider={provider} key={provider} />
       ))} */}
     </div>
   );
 }
-
-export default LoginPage;
